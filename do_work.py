cfa05ddb33d14abeca909a035fffdbc876bcddb2
Justin Wozniak <woz@anl.gov>
2023-01-20 14:44:49 -0600

    Actually execute given string

--- do_work.py
+++ do_work.py
@@ -1,13 +1,13 @@
 
 import sys, time
+import subprocess
 
 def do_work(msg):
     print("do_work: " + msg + " ...")
     sys.stdout.flush()
-    # Last two characters are numbers:
-    t = msg[-2:]
-    delay = int(t) / 10
-    time.sleep(delay)
-    print("do_work: " + msg + " done.")
+    subprocess.call(msg.split())
     sys.stdout.flush()
+
+
+
     return len(msg)
