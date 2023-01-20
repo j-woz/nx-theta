b109c38064e898b4d755333c0a6028f93e3bdf8a
Justin Wozniak <woz@anl.gov>
2023-01-20 14:44:38 -0600

    Note about QUEUE location

--- check_queue.py
+++ check_queue.py
@@ -3,7 +3,11 @@ import os
 
 from nxrefine.nxserver import NXFileQueue
 
-QUEUE = os.getenv("HOME") + "/QUEUE"
+# TODO: Replace with contents of ~/.nxserver/settings.ini
+QUEUE = os.getenv("HOME") + "/NX-Q/nxserver/task_list"
+
+# QUEUE = os.getenv("HOME") + "/QUEUE"
 
 Q = NXFileQueue(QUEUE)
+print(QUEUE)
 print("length: %i" % Q.qsize())
